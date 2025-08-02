6ea6233c1eb8f4c450b622c2b4f19d9ae3df125f
Ashutosh <ashutoshsingh6376@gmail.com>
2025-08-02 11:09:32 +0530

    Work done

---
+++ 2410_leetcode.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int i = 0, j = 0;
+        int matches = 0;
+
+        while (i < players.length && j < trainers.length) {
+            if (players[i] <= trainers[j]) {
+                matches++;
+                i++;
+                j++;
+            } else {
+                j++; // trainer can't handle player, try next trainer
+            }
+        }
+
+        return matches;
+    }
+}
