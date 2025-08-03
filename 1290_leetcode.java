b7b830ff59dc8a08eff0f1f412144b1924f8aaa0
Ashutosh <ashutoshsingh6376@gmail.com>
2025-08-03 23:51:24 +0530

    Work done

---
+++ 1290_leetcode.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+        while (head != null) {
+            result = (result << 1) | head.val;  // Left shift and add current bit
+            head = head.next;
+        }
+        return result;
+    }
+}
